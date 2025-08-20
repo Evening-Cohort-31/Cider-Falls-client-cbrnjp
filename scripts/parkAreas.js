e9422004b89f3e3ec3dacbe8e0c0edf50d28c980
Nicole D'Anton <nicoledanton7@gmail.com>
2025-08-19 20:27:07 -0500

    improvements on event listener

--- scripts/parkAreas.js
+++ scripts/parkAreas.js
@@ -21,13 +21,29 @@ export const createParkAreasHTML = () => {
 }
 
 
+
+//this works because in main.css i added .services--button h3 { pointer-events: none; } which tells h3 to get out of the way of the click event
 document.addEventListener(
     "click",
     (ClickEvent) => {
         const cardClicked = ClickEvent.target
-        const button = ClickEvent.target.closest(".services--button");
-        if (button){
-            window.alert(`The services offered here are ${button.dataset.services}`)
+        if (cardClicked.dataset.type === "services-button"){
+            window.alert(`The services offered here are ${cardClicked.dataset.services}`)
         }
     }
-)
+) 
+
+
+
+//alernate way to do it (makes sure that entire green button can be clicked, and the h3 doesnt get in the way)
+// document.addEventListener(
+//     "click",
+//     (ClickEvent) => {
+//         const cardClicked = ClickEvent.target
+//         const button = ClickEvent.target.closest(".services--button");
+//         if (button){
+//             window.alert(`The services offered here are ${button.dataset.services}`)
+//         }
+//     }
+// )
+
