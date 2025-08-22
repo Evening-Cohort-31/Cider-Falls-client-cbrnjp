6ed52bf6896229214ae0b81ec8a360301388a7da
Jason Norman <json.norman@gmail.com>
2025-08-21 20:48:09 -0500

    Created Guest List function on guests.js

---
+++ scripts/guests.js
@@ -0,0 +1,21 @@
+// Pulls in a function called getGuests from database.js
+import { getGuests } from "./database.js"
+
+// Runs the getGuests function and Stores the returned array of guest objects in the variable guests
+const guests = getGuests()
+
+// Defines and exports a function named Guests, which When called will go through the array (getGuests) and add each guest object found in the array in an unordered list
+export const Guests = () => {
+    let guestHTML = "<ul>"
+
+    for (const guest of guests) {
+        guestHTML += `<li>${guest.name}</li>`
+    }
+
+    guestHTML += "</ul>"
+    return guestHTML
+
+}
+
+
+// will need following on main.js:  import { Guests } from "./guests.js"
