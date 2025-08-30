8e22d6bdcafaa7f64e826cf5b11b57124e93a709
Nicole D'Anton <nicoledanton7@gmail.com>
2025-08-30 11:05:03 -0500

    added event listener to service list and clickable styling

--- scripts/services.js
+++ scripts/services.js
@@ -1,10 +1,18 @@
-import { getServices } from "./database.js";
+import { getParkAreas, getServices } from "./database.js";
 
 const servicesArray = getServices()
 
 
 export const createServicesBulletHTML = () => {
-    const serviceNames = servicesArray.map(service => service.name).join(", ")
+    // Build clickable spans for each service. A span is an in-line element. using a block element like <p> or <li> would force new lines
+    const serviceNames = servicesArray.map(service => 
+        `<span data-type="service" 
+                data-id="${service.id}"
+                data-parkid="${service.parkAreaId}" 
+                class="service-item">
+                ${service.name}
+                </span>`
+    ).join(", ")
 
     const html = `<article>
                     <p><strong>Park Services:</strong> ${serviceNames}</p>
@@ -13,3 +21,25 @@ export const createServicesBulletHTML = () => {
     return html
 }
 
+document.addEventListener(
+    "click",
+    (AnotherClickEvent) => {
+        const serviceClicked = AnotherClickEvent.target
+        if (serviceClicked.dataset.type === "service") {
+            const serviceId = parseInt(serviceClicked.dataset.id)
+        
+        // Find the service
+            const services = getServices()
+            const service = services.find(s => 
+                s.id === serviceId)
+
+        // Get park areas that have this service
+        const parkAreas = getParkAreas()
+        const matchingAreas = parkAreas
+            .filter(area => 
+                service.parkAreaId.includes(area.id))
+            .map(area => area.name)
+
+        window.alert(`The "${service.name}" service is available in: ${matchingAreas.join(", ")}`)
+    }
+})
